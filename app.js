90f6614842349bc77917d88262b77d601bfde5c7
manuelangel0909 <archivosesup@hotmail.com>
2022-03-15 13:34:24 -0300

    empezando los grids

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname,"./views/index.html"));
+  res.sendFile(path.resolve(__dirname,"./views/productDetail.html"));
 
 });
 
