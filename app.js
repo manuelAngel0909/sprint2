3f496cde3954c6b83fbe4c2904357ec7badca400
manuelangel0909 <archivosesup@hotmail.com>
2022-03-21 21:50:19 -0300

    previo a reformular

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname,"./views/product-Detail.html"));
+  res.sendFile(path.resolve(__dirname,"./views/product-detail.html"));
 
 });
 
